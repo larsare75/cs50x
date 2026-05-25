1c8e77404d42c65e18cf0402aebfaa580495db2e
larsare75 <lars.killingdalen@gmail.com>
2026-05-25 19:18:45 +0200

    ummmdone

--- um/test_um.py
+++ um/test_um.py
@@ -1,1 +1,12 @@
-asdfd
+from um import count
+#import pytest
+
+def test_conv():
+    assert count("um") == 1
+    assert count("um?") == 1
+    assert count("Um, thanks for the album") == 1
+    assert count("Um, thanks, um...") == 2
+    
+#def test_wronginput():   
+#    with pytest.raises(ValueError):
+#        convert("9:60 AM to 5:60 PM") 
--- um/um.py
+++ um/um.py
@@ -1,1 +1,14 @@
-sdfdf
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    return len(re.findall(r'\bum\b',s,re.IGNORECASE))
+
+
+if __name__ == "__main__":
+    main()
